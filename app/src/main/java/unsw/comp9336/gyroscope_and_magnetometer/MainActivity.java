d0cee194971622fe2ac95761301337645ddfd981
Jack Jiang <jackjyq@outlook.com>
2019-07-20 18:21:37 +1000

    add UI layout

---
+++ app/src/main/java/unsw/comp9336/gyroscope_and_magnetometer/MainActivity.java
@@ -0,0 +1,113 @@
+package unsw.comp9336.gyroscope_and_magnetometer;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String github_url = getResources().getString(R.string.author_website);
+                Intent openUrlIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(github_url));
+                startActivity(openUrlIntent);
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Task1Fragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_task1);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new AboutFragment()).commit();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_task1) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Task1Fragment()).commit();
+        } else if (id == R.id.nav_task2) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Task2Fragment()).commit();
+        } else if (id == R.id.nav_task3) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Task3Fragment()).commit();
+        } else if (id == R.id.nav_task4) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Task4Fragment()).commit();
+        } else if (id == R.id.nav_task5) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Task5Fragment()).commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/test/java/unsw/comp9336/gyroscope_and_magnetometer/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package unsw.comp9336.gyroscope_and_magnetometer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
